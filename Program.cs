e23956e11e5887aac544c78d67a22af5ab2778ba
mustafaberk01 <mustafaberkay5234@gmail.com>
2023-01-29 19:41:55 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Program
+    {
+        static Boolean hasMatchingParentheses(string s)
+        {
+            int leftBracketCount = 0;
+            int rightBracketCount = 0;
+
+            if (string.IsNullOrEmpty(s))
+            {
+                return false;
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    leftBracketCount++;
+                }
+                else if (s[i] == ')')
+                {
+                    rightBracketCount++;
+                }
+            }
+
+            if (leftBracketCount == rightBracketCount)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(hasMatchingParentheses("((hello()))"));
+            Console.WriteLine(hasMatchingParentheses("()(hello())"));
+            Console.WriteLine(hasMatchingParentheses("((hello))"));
+            Console.WriteLine(hasMatchingParentheses("(hello)"));
+
+            Console.WriteLine();
+
+            Console.WriteLine(hasMatchingParentheses("(hello("));
+            Console.WriteLine(hasMatchingParentheses(")hello)"));
+            Console.WriteLine(hasMatchingParentheses(")hello("));
+            Console.WriteLine(hasMatchingParentheses("hello(("));
+            Console.WriteLine(hasMatchingParentheses("(hello"));
+            Console.WriteLine(hasMatchingParentheses("((hello)"));
+        }
+    }
